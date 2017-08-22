2fe3e9f8a655f8d5a6e0964528608dce7af01a7a
Chris <chrisg32@gmail.com>
2017-08-21 22:14:25 -0500

    Tab menu coming along

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,56 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Stratus.ViewModels;
 
 namespace Stratus
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
-            this.InitializeComponent();
-            ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
-            formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            InitializeComponent();
+
+            DataContext = new WindowViewModel();
+
             CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBarOnLayoutMetricsChanged;
+
+            TitleBar.Height = coreTitleBar.Height;
             coreTitleBar.ExtendViewIntoTitleBar = true;
 
-            
+            //Window.Current.SetTitleBar is used to identify which control will handle user inputs (Grab and move)
+            Window.Current.SetTitleBar(SystemButtonGutter);
+
+            var view = ApplicationView.GetForCurrentView();
+
+            view.TitleBar.BackgroundColor = Colors.DarkGreen;
+            view.TitleBar.ForegroundColor = Colors.White;
+
+            view.TitleBar.ButtonBackgroundColor = Colors.DarkGreen;
+            view.TitleBar.ButtonForegroundColor = Colors.White;
+
+            view.TitleBar.ButtonHoverBackgroundColor = Colors.Green;
+            view.TitleBar.ButtonHoverForegroundColor = Colors.White;
+
+            view.TitleBar.ButtonPressedBackgroundColor = Color.FromArgb(255, 0, 120, 0);
+            view.TitleBar.ButtonPressedForegroundColor = Colors.White;
+
+            view.TitleBar.ButtonInactiveBackgroundColor = Colors.DarkGray;
+            view.TitleBar.ButtonInactiveForegroundColor = Colors.Gray;
+
+            view.TitleBar.InactiveBackgroundColor = Colors.DarkGreen;
+            view.TitleBar.InactiveForegroundColor = Colors.Gray;
+
         }
 
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        private void CoreTitleBarOnLayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
         {
-            Browser.Navigate(new Uri("https://www.guitartricks.com"));
-
-
-
-//            if (ApplicationView.GetForCurrentView().IsViewModeSupported(ApplicationViewMode.CompactOverlay))
-//            {
-//                ViewModePreferences compactOptions = ViewModePreferences.CreateDefault(ApplicationViewMode.CompactOverlay);
-//                compactOptions.CustomSize = new Windows.Foundation.Size(320, 200);
-//                var result = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay, compactOptions);
-//            }
-
+            TitleBar.Height = sender.Height;
+            SystemButtonGutter.Width = sender.SystemOverlayRightInset;
+            MagicButton.Width = sender.SystemOverlayRightInset / 4;
         }
 
+        private async void DoPictureInPicture()
+        {
+            if (ApplicationView.GetForCurrentView().IsViewModeSupported(ApplicationViewMode.CompactOverlay))
+            {
+                ViewModePreferences compactOptions = ViewModePreferences.CreateDefault(ApplicationViewMode.CompactOverlay);
+                compactOptions.CustomSize = new Windows.Foundation.Size(320, 200);
+                var result = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay, compactOptions);
+            }
+        }
+
+ 
     }
 }
---
+++ ViewModels/WindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Windows.Mvvm;
+
+namespace Stratus.ViewModels
+{
+    class WindowViewModel : ViewModelBase
+    {
+        private TabViewModel _currentTab;
+        public ObservableCollection<TabViewModel> Tabs { get; }
+
+        public TabViewModel CurrentTab
+        {
+            get => _currentTab;
+            set => SetProperty(ref _currentTab, value);
+        }
+
+        public ICommand NewTabCommand { get; }
+
+        public WindowViewModel()
+        {
+            Tabs = new ObservableCollection<TabViewModel>();
+            NewTabCommand = new DelegateCommand(ExecuteNewTab);
+
+            ExecuteNewTab();
+            CurrentTab = Tabs.First();
+        }
+
+        private void ExecuteNewTab()
+        {
+            Tabs.Add(new TabViewModel
+            {
+                Title = Faker.Company.Bullshit()
+            });
+        }
+    }
+}
