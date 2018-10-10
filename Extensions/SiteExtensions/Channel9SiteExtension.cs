d1639d0756248e90e8c0a2a06ee8d86b35fc25a9
Chris Gonzales <chrisg32@gmail.com>
2018-10-09 20:49:13 -0500

    Online Stopwatch support
    Site extension for online-stopwatch.com #21

--- Extensions/SiteExtensions/Channel9SiteExtension.cs
+++ Extensions/SiteExtensions/Channel9SiteExtension.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Web;
 using HtmlAgilityPack;
 
 namespace Stratus.Extensions.SiteExtensions
---
+++ Extensions/SiteExtensions/OnlineStopwatchSiteExtension.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Stratus.Extensions.SiteExtensions
+{
+    class OnlineStopwatchSiteExtension : BaseSiteExtension
+    {
+        #region Overrides of BaseSiteExtension
+
+        public override bool Filter(string url)
+        {
+            return Regex.IsMatch(url, @"^(?:https?:\/\/)?(?:www\.)?online-stopwatch\.com\/.*", RegexOptions.IgnoreCase);
+        }
+
+        public override string Name => "Online-Stopwatch.com";
+        public override string Description => "Allows online-stopwatch.com to run in PIP mode.";
+        public override string IconUrl => @"https://www.online-stopwatch.com/favicon.ico";
+
+        #endregion
+
+        public override async Task OnPictureInPicture(Document document)
+        {
+            await RedirectToPopoutVersion(document);
+        }
+
+        public override async Task OnFullScreen(Document document)
+        {
+            await RedirectToPopoutVersion(document);
+        }
+
+        private async Task RedirectToPopoutVersion(Document document)
+        {
+            var vector = await document.RunJavascript(@"OSapp.vector");
+            if (!string.IsNullOrWhiteSpace(vector))
+            {
+                document.Navigate(@"https://www.online-stopwatch.com/html5/" + vector);
+            }
+        }
+    }
+}
